e8294171b1da8bb904cc63a06d72f9df43a08a7d
Maria Fernanda Diaz Burgos <mafe19679@gmail.com>
2024-04-15 17:09:28 -0500

    Vista Administrador OK

--- JS/admin.js
+++ JS/admin.js
@@ -1,5 +1,37 @@
+// Cambiar info del modal cuando sea editar o crear
 function alterbuttons(accion, tipo) {
     document.getElementById("titulo" + tipo).innerHTML = accion + " " + tipo
     document.getElementById("boton" + tipo).innerHTML = accion
     $("#nuevo" + tipo).modal("show")
+}
+
+// Confirmación elimar un registro
+function mostrarConfirmacion(e) {
+    Swal.fire({
+        title: '¿Estás seguro de eliminar este registro?',
+        //   text: 'Esta acción es irreversible',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí',
+        cancelButtonText: 'No'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire('Eliminado', 'El registro ha sido eliminado', 'success');
+            console.log(e, result)
+        } else {
+            Swal.fire('Cancelado', 'La acción ha sido cancelada', 'error');
+        }
+    });
+}
+
+// Responder por email mensaje de contactenos
+function enviarCorreo() {
+    var correo = document.getElementById("correo").innerHTML;
+    var asunto = "Respuesta a tu consulta FRAME://WORK";
+    var cuerpoMensaje = "Hola, \n";
+    cuerpoMensaje += "De acuerdo a tu consulta te contamos que: \n\n";
+    cuerpoMensaje += "--- EDITA EL MENSAJE ACÁ --- \n\n";
+    cuerpoMensaje += "Gracias por contáctarnos.\n Equipo FRAME://WORK"
+    var enlaceCorreo = "mailto:" + correo + "?subject=" + encodeURIComponent(asunto) + "&body=" + encodeURIComponent(cuerpoMensaje);
+    window.location.href = enlaceCorreo;
 }
